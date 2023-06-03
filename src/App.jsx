c5d3a671945a956f3abec0c444a8c3a5035e3fea
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-06-03 09:39:17 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Pages/Routes/Routes';
 
 function App() {
   return (
-    <div >
-   <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}>
 
-<button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+      </RouterProvider>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+
+
+  daisyui: {
+    themes: [
+      {
+        airlines: {
+        
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: "#3A4256",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
+
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
